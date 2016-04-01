c937a6b36919285a6d2e920042fce5d87d890133
Eugene Karanda <eugene.karanda@gmail.com>
2016-04-02 01:09:41 +0300

    Implement step 2 Add tutorial module and controller for phones Show list of phones in index page

---
+++ app/js/controllers.js
@@ -0,0 +1,22 @@
+/**
+ * Created by eugene.karanda on 02.04.2016.
+ */
+
+var tutorialApplication = angular.module('tutorialApplication', []);
+
+tutorialApplication.controller('phoneListController', function ($scope) {
+    $scope.phones = [
+        {
+            'name': 'Nexus S',
+            'snippet': 'Fast just got faster with Nexus S.'
+        },
+        {
+            'name': 'Motorola XOOM™ with Wi-Fi',
+            'snippet': 'The Next, Next Generation tablet.'
+        },
+        {
+            'name': 'MOTOROLA XOOM™',
+            'snippet': 'The Next, Next Generation tablet.'
+        }
+    ];
+});
