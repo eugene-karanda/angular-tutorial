f551abdf566b3b718c2a39a5322748a370719782
eugene.karanda <eugene.karanda@gmail.com>
2016-04-03 03:22:41 +0300

    Implement sorting(without page styling and testing)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,15 +8,18 @@ tutorialApplication.controller('PhoneListController', function ($scope) {
     $scope.phones = [
         {
             'name': 'Nexus S',
-            'snippet': 'Fast just got faster with Nexus S.'
+            'snippet': 'Fast just got faster with Nexus S.',
+            'age': 1
         },
         {
             'name': 'Motorola XOOM™ with Wi-Fi',
-            'snippet': 'The Next, Next Generation tablet.'
+            'snippet': 'The Next, Next Generation tablet.',
+            'age': 2
         },
         {
             'name': 'MOTOROLA XOOM™',
-            'snippet': 'The Next, Next Generation tablet.'
+            'snippet': 'The Next, Next Generation tablet.',
+            'age': 3
         }
     ];
     
@@ -28,5 +31,7 @@ tutorialApplication.controller('PhoneListController', function ($scope) {
         } else {
             $scope.title = 'Phone Gallery';
         }
-    })
+    });
+    
+    $scope.orderProperty = 'age';
 });
