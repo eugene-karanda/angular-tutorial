37597899783c9c62c99c09ae8e77f4c81f031f61
eugene.karanda <eugene.karanda@gmail.com>
2016-04-03 04:09:57 +0300

    fix watch in tests

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,13 +2,13 @@
  * Created by eugene.karanda on 02.04.2016.
  */
 
-describe('PhoneListController', function () { //TODO try rename to controller spec
+describe('PhoneListController', function () {
 
     beforeEach(module('tutorialApplication'));
 
-    it('should create "phones" model with 3 phones', inject(function ($controller) {
-        var scope = {};
-        $controller('phoneListController', {$scope: scope});
+    it('should create "phones" model with 3 phones', inject(function ($rootScope, $controller) {
+        var scope = $rootScope.$new();
+        $controller('PhoneListController', {$scope: scope});
 
         expect(scope.phones.length).toBe(3);
     }));
